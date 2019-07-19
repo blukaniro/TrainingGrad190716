c727e21d2319849ac77e84b6d304298211edaf5d
TAJIMA Ryosuke <>
2019-07-19 12:42:20 +0900

    up

--- DataAnalysis/analysis.r
+++ DataAnalysis/analysis.r
@@ -1,6 +1,8 @@
+# 190719田島亮介
 # データの読み込みと整理
 d<-read.table("dataset.txt", header=T)
 d2<-d[c(-4,-11),] #出液とSFWが著しく大きい値のものを外れ値として除外
+
 
 # 分散分析
 summary(aov(sap~treatment,d2))
@@ -10,11 +12,13 @@ summary(aov(root~treatment,d2))
 ## 新鮮重: 10%水準で，慣行>有機，節根数：5%水準で慣行>有機
 ## これらを栽培方法との関係から考察．あるいは有意差なしの考察(をしないと出液速度に触れられない？)
 
+
 # 相関のチェック
 plot(d2[,2:5])
 cor(d2[,2:5])
 ## 新鮮重，茎数，節根数の相関高い
 ## これらの考察
+
 
 # データから指標算出
 root_per_panicle<-d2$root/d2$panicle #茎数あたりの節根数
@@ -22,7 +26,8 @@ sap_per_panicle<-d2$sap/d2$panicle #茎数あたりの出液速度
 sap_per_root<-d2$sap/d2$root #根数あたりの出液速度
 d2<-data.frame(d2,rp=root_per_panicle, sp=sap_per_panicle, sr=sap_per_root) #データ追加
 
- # 分散分析
+
+# 分散分析
 summary(aov(rp~treatment,d2))
 summary(aov(sp~treatment,d2))
 summary(aov(sr~treatment,d2))
@@ -31,8 +36,8 @@ summary(aov(sr~treatment,d2))
 ## 根数あたりの出液速度: 1%水準で慣行<有機
 ## この関係を栽培方法との関係から考察
 
-## 図表は箱ひげ図，棒グラフ，相関図等がわかりやすく表記され適切に言及されている
 
+# 図表は箱ひげ図，棒グラフ，相関図等がわかりやすく表記され適切に言及されている
 par(mfrow=c(2,4))
 boxplot(sap~treatment,d2)
 boxplot(SFW~treatment,d2)
